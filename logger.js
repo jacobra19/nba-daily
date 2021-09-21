c7ed86b9b3d1b5c83837d067160d5bd163b583a2
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-21 15:53:34 +0300

    logger.js

---
+++ logger.js
@@ -0,0 +1,29 @@
+import CFonts from 'cfonts';
+import chalk from 'chalk';
+
+const logHeader = (title, options = {}) => {
+    const cFontsOptions = {
+        font: 'block', // define the font face
+        align: 'left', // define text alignment
+        colors: ['blue'], // define all colors
+        background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+        // letterSpacing: 1,           // define letter spacing
+        // lineHeight: 1,              // define the line height
+        // space: true,                // define if the output text should have empty lines on top and on the bottom
+        // maxLength: '0',             // define how many character can be on one line
+        // gradient: false,            // define your two gradient colors
+        // independentGradient: false, // define if you want to recalculate the gradient for each new line
+        // transitionGradient: false,  // define if this is a transition between colors directly
+        // env: 'node'                 // define the environment CFonts is being executed in
+        ...options,
+    };
+
+    CFonts.say(title, cFontsOptions);
+};
+
+const logInfo = (message, options = {}) => {
+    console.log(chalk.bgBlueBright.white(message));
+    // console.log(chalk.bgWhite.blueBright(message));
+};
+
+export { logHeader, logInfo };
