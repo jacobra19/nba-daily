d9b69ca23da77d39ce4a167acb06b6e88c63990c
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-22 00:07:02 +0300

    main flow

--- cli.js
+++ cli.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
-
-import { getDocument, getDateMessage } from './functions.js';
+import { getDocument, getDateMessage, parseAndLogTables } from './functions.js';
 import { logHeader, logInfo } from './logger.js';
 const [, , ...args] = process.argv;
 
 const init = async () => {
     const document = await getDocument(
-        'https://www.basketball-reference.com/boxscores'
+        'https://www.basketball-reference.com/boxscores/?month=4&day=19&year=2021'
     );
     const dateMessage = getDateMessage(document);
-    logHeader('nba');
+    logHeader('nba-daily');
     logInfo(dateMessage);
-    // TODO: parse the html
-    // TODO: display the date
+    parseAndLogTables(document);
 };
 
 init();
