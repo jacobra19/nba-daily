5bd18c21c5e982eb5746641d5376d750fc192b0f
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-25 03:19:44 +0300

    --date param added

--- cli.js
+++ cli.js
@@ -1,18 +1,24 @@
 #!/usr/bin/env node
-import { getDocument, getDateMessage, parseAndLogTables } from './functions.js';
+import { getDocument, parseAndLogTables, getDateQuery } from './functions.js';
 import { logHeader, logInfo } from './logger.js';
-const [, , ...args] = process.argv;
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+
+const { date } = yargs(hideBin(process.argv)).argv;
 
 // 'https://www.basketball-reference.com/boxscores/?month=5&day=19&year=2019' // 2OT EXAMPLE
 // 'https://www.basketball-reference.com/boxscores/?month=4&day=19&year=2021' // 9 games EXAMPLE
 
+
+
 const init = async () => {
+    const dateQuery = getDateQuery(date);
     const document = await getDocument(
-        'https://www.basketball-reference.com/boxscores'
+        `https://www.basketball-reference.com/boxscores/${dateQuery}`
     );
-    const dateMessage = getDateMessage(document);
+
     logHeader('nba-daily');
-    logInfo(dateMessage);
     parseAndLogTables(document);
 };
 
